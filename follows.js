4b6cc08891cdef6f2abfdafd9a3b89b27997c26d
chil020 <carrick.h@gmail.com>
2013-08-29 07:07:13 +1200

    Update follows.js

--- follows.js
+++ follows.js
@@ -322,7 +322,7 @@
 							$('#feed-' + data.id + ' .map').addClass('hidden');
 					}
 
-					$('#feed-' + data.id + ' .duration-hour').click(function() {
+					$('#feed-' + data.id + ' .duration-test').click(function() {
 						$('#loadingData').foundation('reveal', 'open');
 						updateFeeds(data.id, thisFeedDatastreams, 'TEST', 0);
 						return false;
